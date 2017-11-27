c693609be58544e6af8a3a239b478a48fa94bb0a
bleung329 <bleung@stuy.edu>
2017-11-27 09:06:34 -0500

    I cant get an IBM Watson key right now. I'll have to work on app.py later.

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
+#!usr/bin/python
 __authors__ = ['Khyber Sen', 'Bayan Berri', 'Naotaka Kinoshita', 'Brian Leung']
 __date__ = '2017-10-30'
 
+from flask import Flask, session, render_template, request, redirect, url_for, flash
 from core import app
 
+@app.route("/")
+def landing():
+	render_template("welcome.jinja2")
+
 if __name__ == '__main__':
+    app.debug = True
     app.run()
